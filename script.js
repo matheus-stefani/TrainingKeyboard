cf5029899746c33d9eec0ae5aea62bf7003892a9
MATHEUS STEFANI DOS SANTOS <41953@eep.br>
2024-08-08 17:58:11 -0300

    Hash and Correct keys

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const numberCharText = 100
 const charArray = ['!','@','\'','\"','#','$','%','&','*','(',')',
                         '-','_','+','=','[',']','{','}','<','>','.',':',';',',','\\',
                         "ArrowRight", "ArrowLeft","ArrowUp","ArrowDown","Delete","Home",
-                        "End"];
+                        "End", "\|"];
+
+const charArrayMap = new Map()
+
+
 
 const GetRandomChars = (min, max) => charArray[Math.floor(Math.random() * (max - min)) + min]
 let currentChar = 0;
@@ -11,22 +15,31 @@ let currentChar = 0;
 window.onload = () =>{
     
     createLabels();
+    
     const getLabels = document.querySelectorAll("label");
     
     document.addEventListener('keydown',(e)=>{
         let { key } = e
-        getLabels[currentChar].classList.add("gray")
         console.log(key)
-        currentChar++
+
+        if(charArrayMap.has(key) || (Number(key)>=0 && Number(key)<10) ){
+           getLabels[currentChar].classList.add("gray")
+           currentChar++
+        }
+
     })
 }
 
 
 const createLabels = () =>{
     for(let i = 0 ; i < numberCharText ; i++){
+
+        if(i < charArray.length) charArrayMap.set(charArray[i])
+
         const createLabel = document.createElement("label");
-        createLabel.innerText = GetRandomChars(0,charArray.length-1) + " "
+        createLabel.innerText = GetRandomChars(0,charArray.length) + " "
         const getMainDiv = document.querySelector(".main")
         getMainDiv.appendChild(createLabel);
     }
 }
+
