3a4b600d29c6a858761c8af7a6075dda6df8aaee
MATHEUS STEFANI DOS SANTOS <41953@eep.br>
2024-08-09 22:33:09 -0300

    Backspace and green, red and gray

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const numberCharText = 100
 const charArray = ['!','@','\'','\"','#','$','%','&','*','(',')',
                         '-','_','+','=','[',']','{','}','<','>','.',':',';',',','\\',
                         "ArrowRight", "ArrowLeft","ArrowUp","ArrowDown","Delete","Home",
-                        "End", "\|"];
-
+                        "End", "\|","\/"];
+charArrayInOrder = []
 const charArrayMap = new Map()
 
 
@@ -14,32 +14,52 @@ let currentChar = 0;
 
 window.onload = () =>{
     
-    createLabels();
-    
+    createLabelsAndHash();
+
     const getLabels = document.querySelectorAll("label");
     
     document.addEventListener('keydown',(e)=>{
         let { key } = e
-        console.log(key)
+        if(key=="Backspace" && currentChar > 0 && getLabels[currentChar-1].classList == "red"){
+            
+            getLabels[currentChar].classList.remove("gray")
+            currentChar--
+            getLabels[currentChar].classList.remove("red")
+            getLabels[currentChar].classList.add("gray")
 
+        }
         if(charArrayMap.has(key) || (Number(key)>=0 && Number(key)<10) ){
-           getLabels[currentChar].classList.add("gray")
-           currentChar++
+           
+           if(charArrayInOrder[currentChar]==key){
+            getLabels[currentChar].classList.remove("gray")
+            getLabels[currentChar].classList.add("green")
+           } 
+           else{
+            getLabels[currentChar].classList.remove("gray")
+            getLabels[currentChar].classList.add("red")
+           }
+        currentChar++               
+        getLabels[currentChar].classList.add("gray")
         }
 
     })
 }
 
 
-const createLabels = () =>{
+const createLabelsAndHash = () =>{
     for(let i = 0 ; i < numberCharText ; i++){
 
         if(i < charArray.length) charArrayMap.set(charArray[i])
 
         const createLabel = document.createElement("label");
-        createLabel.innerText = GetRandomChars(0,charArray.length) + " "
+        charArrayInOrder[i] = GetRandomChars(0,charArray.length)
+        createLabel.innerText = charArrayInOrder[i]
         const getMainDiv = document.querySelector(".main")
+        if(i==0) createLabel.classList.add("gray")
         getMainDiv.appendChild(createLabel);
+
+        if(i%13==0 && i != 0) getMainDiv.appendChild(document.createElement("br"));
+        
     }
 }
 
